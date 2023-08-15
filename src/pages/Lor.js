eaa1acf2bd6f463320d93ebc58ea1a36ce030f5c
wonko <jeffshen.37@gmail.com>
2023-08-14 20:25:21 -0400

    backend link complete

--- src/pages/Lor.js
+++ src/pages/Lor.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 function Lor() {
+  const [data, setData] = useState([])
+  
+  useEffect(() => {
+    fetch('/members')
+    .then(response => response.json()
+    .then(data => {
+      console.log(data)
+      setData(data)
+    })
+  )}, []);
+
   return (
     <div className = 'Lor'>
-        LOR
+        icky
     </div>
   )
 }
