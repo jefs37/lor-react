ef0035056ec593d5b458b45688eff6433d0ef4bb
jeff <jeffshen.37@gmail.com>
2023-08-16 18:37:12 -0400

    riot api working

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,10 +1,49 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import requests
+
+with open('./flask-server/apikey.txt', 'r') as f:
+    apiKey = f.read()
+
+def getPlayer(name):
+    apiUrl = ("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" 
+              + name + "?api_key=" + apiKey)
+    resp = requests.get(apiUrl)
+    return resp.json()
+
+def getMatchIDs(name, puuid):
+    apiUrl = ("https://americas.api.riotgames.com/lor/match/v1/matches/by-puuid/"
+              + puuid + "/ids?start=0&count=10&api_key=" + apiKey)
+    resp = requests.get(apiUrl)
+    return resp.json()
+
+def getMatchInfo(match):
+    apiUrl = ("https://americas.api.riotgames.com/lor/match/v1/matches/"
+              + match + "?api_key=" + apiKey)
+    resp = requests.get(apiUrl)
+    return resp.json()
 
 app = Flask(__name__)
 
-@app.route('/members')
-def members():
-    return 'abc'
+@app.route('/userinfo')
+def display_user_info():
+    return '{"stats" : "xyz"}'
+
+@app.route('/finduser', methods = ['GET', 'POST'])
+def get_user():
+    username = request.get_json()
+    if username == None:
+        return jsonify('no results')
+    else:
+        name = username.get('username')
+        print(name)
+        puuid = (getPlayer(name))["puuid"]
+        matches = getMatchIDs(name, puuid)
+        info = [0] * 20
+        for i in range(0,10):
+            info[i] = getMatchInfo(matches[i])
+    print(info)
+    return jsonify(info)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
--- src/pages/Lor.js
+++ src/pages/Lor.js
@@ -1,20 +1,53 @@
 import React, {useState, useEffect} from 'react'
 
 function Lor() {
-  const [data, setData] = useState([])
+  const [stats, setStats] = useState('')
+  const [username, setUsername] = useState('')
   
-  useEffect(() => {
-    fetch('/members')
+/*  useEffect(() => {
+    fetch('/finduser')
     .then(response => response.json()
     .then(data => {
       console.log(data)
-      setData(data)
+      setStats(data.metadata)
     })
-  )}, []);
+    )}, []);
+*/
+  const userSubmit = (e) => {
+    e.preventDefault()
+    const userQuery = {username}
+
+    fetch('/finduser', {
+      method: 'POST',
+      headers: {"Content-type" : "application/json"},
+      body: JSON.stringify(userQuery)
+    }).then(
+      response => response.json()
+      ).then(
+        data => {
+          console.log(data)
+          setStats(data)
+        }
+      )
+  }
 
   return (
     <div className = 'Lor'>
-        icky
+      <h1>last played deck search</h1>
+      <form onSubmit={userSubmit}>
+        <label>Username:</label>
+        <input
+          type = 'text'
+          required
+          value = {username}
+          onChange = {(e) => setUsername(e.target.value)}
+        />
+        <button>search user</button>
+      </form>
+      <p></p>
+      {username} stats
+      <p></p>
+      {JSON.stringify(stats)}
     </div>
   )
 }
